ad20618be4f39794b5926195dd768a6605e9cfaa
Aleksandr <sky7way@yandex.ru>
2023-05-27 01:28:12 +0300

    MouseEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
+    const addButton = (event:MouseEvent<HTMLButtonElement>) => {
+        console.log('Hello!')
+    }
+    const deleteButton = (event:MouseEvent<HTMLButtonElement>) => {
+        console.log('Chao!')
+    }
+
     return (
         <div className="App">
-            <button onClick={(event) => {
-                console.log('Hello!')
-            }}>Add button
+            <button onClick={ addButton }>Add button
             </button>
-            <button onClick={(event) => {
-                console.log('Chao!')
-            }}>Delete button
+            <button onClick={ deleteButton }>Delete button
             </button>
         </div>
     );
