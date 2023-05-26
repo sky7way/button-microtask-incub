0fa395429e8a5d09682c71e01ea1a02e395f15a4
Aleksandr <sky7way@yandex.ru>
2023-05-27 01:56:10 +0300

    onClickHello-onClickGoodbye

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-    const addButton = (event:MouseEvent<HTMLButtonElement>) => {
-        console.log('Hello!')
+    // const addButton = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello!')
+    // }
+    // const deleteButton = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Chao!')
+    // }
+
+    const onClickHello = (name:string) => {
+        console.log(`Hello, ${name}!`);
     }
-    const deleteButton = (event:MouseEvent<HTMLButtonElement>) => {
-        console.log('Chao!')
+
+    const onClickGoodbye = (name:string) => {
+        console.log(`Goodbye, ${name}!`);
     }
 
     return (
         <div className="App">
-            <button onClick={ addButton }>Add button
+            <button onClick={ () => onClickHello('Vasya') }>Add button
             </button>
-            <button onClick={ deleteButton }>Delete button
+            <button onClick={ () => onClickGoodbye('Petya') }>Delete button
             </button>
         </div>
     );
