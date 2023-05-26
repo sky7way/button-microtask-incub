e265c9348f7e82f4c0722d27fff9c8d52b7f63a5
Aleksandr <sky7way@yandex.ru>
2023-05-27 02:17:41 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,22 @@ function App() {
         console.log(`Goodbye, ${name}!`);
     }
 
+    const foo1 = () => {
+        console.log(100200);
+    }
+
+    const foo2 = (number: number) => {
+        console.log(number);
+    }
+
     return (
         <div className="App">
             <button onClick={ () => onClickHello('Vasya') }>Add button
             </button>
             <button onClick={ () => onClickGoodbye('Petya') }>Delete button
             </button>
+            <button onClick={ foo1 }>+</button>
+            <button onClick={ () => foo2(100200) }>-</button>
         </div>
     );
 }
